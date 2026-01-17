7945fbe9e82342a312f1b17873ec7f98531a7ce6
Nikhil <nsarathy3@gmail.com>
2026-01-17 17:37:27 -0600

    Last workign version with checkbox and box

--- AngularProject/src/app/app.ts
+++ AngularProject/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatCheckboxModule],
+  imports: [RouterOutlet, MatCheckboxModule, MatButtonModule],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
