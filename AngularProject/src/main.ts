9b95bc4316a288b0c6e9c72a384490348f03c713
Nikhil <nsarathy3@gmail.com>
2026-01-17 15:46:40 -0600

    MAximm Call STack Exceeded error

--- AngularProject/src/main.ts
+++ AngularProject/src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import '@material/web/button/filled-button.js';
+import '@material/web/button/outlined-button.js';
+import '@material/web/checkbox/checkbox.js';
+
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
